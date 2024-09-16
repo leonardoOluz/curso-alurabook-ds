761b689cba8b486075bfa3d2e9a2ceb2646a9659
Leonardo Luz <leonardoluz10@hotmail.com>
2024-09-16 16:03:38 -0300

    Inicializando projeto

---
+++ src/stories/AbBotao.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { AbBotao } from "../componentes/AbBotao";
+
+export default {
+  title: "AbBotao",
+  component: AbBotao,
+} as Meta;
+
+export const Default: StoryObj = {};
